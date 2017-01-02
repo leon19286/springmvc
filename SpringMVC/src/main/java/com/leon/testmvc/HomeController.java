9f91295c1713571010251ce5cdd377a015b88ce7
Victor Leonenko <lvvj@mail.ru>
2017-01-02 12:12:05 +0200

    fixed

--- SpringMVC/src/main/java/com/leon/testmvc/HomeController.java
+++ SpringMVC/src/main/java/com/leon/testmvc/HomeController.java
@@ -27,6 +27,7 @@ public class HomeController {
 	public static void main(String[] args) {
 		System.out.println("1");
 		System.out.println("2");
+		System.out.println("3");
 		System.out.println("4");
 	}
 	/**
