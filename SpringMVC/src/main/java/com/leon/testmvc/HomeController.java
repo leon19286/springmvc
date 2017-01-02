0543eec233cbe335c0623ee0a9f79d86c0d41095
Victor Leonenko <lvvj@mail.ru>
2017-01-02 14:01:57 +0200

    test2

--- SpringMVC/src/main/java/com/leon/testmvc/HomeController.java
+++ SpringMVC/src/main/java/com/leon/testmvc/HomeController.java
@@ -34,6 +34,7 @@ public class HomeController {
 		System.out.println("7");
 		System.out.println("8");
 		System.out.println("9");
+		System.out.println("10");
 	}
 	/**
 	 * Simply selects the home view to render by returning its name.
