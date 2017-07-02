844e892756488d21a5065c50391e0f2f0048a833
Victor Leonenko <lvvj@mail.ru>
2017-07-02 14:44:41 +0300

    1

--- SpringMVC/src/main/java/com/leon/testmvc/HomeController.java
+++ SpringMVC/src/main/java/com/leon/testmvc/HomeController.java
@@ -32,6 +32,8 @@ public class HomeController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
+		logger.info("Welcome home! The client locale is {1}.", locale);
+		logger.info("Welcome home! The client locale is {2}.", locale);
 		//test
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
